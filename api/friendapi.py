fe23f4f59fa67dee9003cd549e3ba18d1d191284
lumokato <suzto@qq.com>
2022-08-15 19:50:34 +0800

    新手任务

--- api/friendapi.py
+++ api/friendapi.py
@@ -9,6 +9,17 @@ class FriendApi(BaseApi):
             # print('发送好友申请成功')
             return True
 
+    # 好友申请列表
+    def pending_list(self):
+        pending_list = []
+        friend_num = 0
+        temp = self.client.callapi('friend/pending_list', {})
+        if 'pending_list' in temp:
+            for user in temp['pending_list']:
+                pending_list.append(user['viewer_id'])
+            friend_num = temp['friend_num']
+        return pending_list, friend_num
+
     # 好友批准
     def friend_accept(self, viewer_id: int):
         self.client.callapi('friend/accept', {'target_viewer_id': viewer_id})
@@ -43,3 +54,11 @@ class FriendApi(BaseApi):
                 if int(reward['id']) == 91002:
                     jewel_accept += int(reward['count'])
         print('    任务已获得钻石' + str(jewel_accept))
+
+    # 改名
+    def rename(self):
+        self.client.callapi('profile/rename', {'user_name': '加完删'})
+
+    # 改简介
+    def update_comment(self, comment):
+        self.client.callapi('profile/update_comment', {'user_comment': comment})
---
+++ friend.py
@@ -0,0 +1,50 @@
+from apscheduler.schedulers.background import BlockingScheduler
+from api.friendapi import FriendApi
+from log import logger
+from json import load, dump
+from os.path import exists
+
+if exists('account.json'):
+    with open('account.json', encoding='utf-8') as fp:
+        total = load(fp)
+
+if exists('friend.json'):
+    with open('friend.json', encoding='utf-8') as fp:
+        friend = load(fp)
+
+if exists('bind.json'):
+    with open('bind.json', encoding='utf-8') as fp:
+        bind = load(fp)
+
+
+def save_friend():
+    global friend
+    with open('friend.json', 'w', encoding='utf-8') as fp:
+        dump(friend, fp, indent=4, ensure_ascii=False)
+
+
+def friend_mission():
+    log = logger('friend')
+    try:
+        for i, friend_account in enumerate(friend["accounts"]):
+            App = FriendApi(friend_account['vid'], friend_account['uid'], total['access_key'])
+            pending_list, friend_num = App.pending_list()
+            if i == 0:
+                if friend_num > 25:
+                    App.friend_remove_all()
+            else:
+                App.friend_remove_all()
+            if pending_list:
+                print(str(App.viewer_id)+'通过好友'+str(pending_list))
+            for pd_user in pending_list:
+                App.friend_accept(pd_user)
+                friend["accounts"][i]['total_friend'] += 1
+        save_friend()
+    except Exception as e:
+        log.exception(e)
+
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler(timezone="Asia/Shanghai")
+    scheduler.add_job(friend_mission, 'cron', second='5')
+    scheduler.start()
