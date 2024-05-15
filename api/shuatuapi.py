bd67254737d7a0a0e5c941cdeac1984e4c0ea425
sun <suzto@outlook.com>
2024-05-15 20:44:40 +0800

    修改买体次数

--- api/shuatuapi.py
+++ api/shuatuapi.py
@@ -238,7 +238,7 @@ class ShuatuApi(BaseApi):
     # 按账号解锁状态刷N图
     async def shuatu_N(self, n_event):
         if n_event == 3:
-            buy_stamina = 6
+            buy_stamina = 3
         else:
             buy_stamina = 0
         quest_n = []
